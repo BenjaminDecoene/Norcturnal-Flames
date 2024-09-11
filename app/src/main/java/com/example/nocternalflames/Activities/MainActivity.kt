85aec7b751484a0076ea7f8de7df731c7c083578
BenjaminDecoene <benjamindecoene@hotmail.com>
2024-09-11 09:46:30 +0200

    feat: add timer, add start game button, add reset button

--- app/src/main/java/com/example/nocternalflames/Activities/MainActivity.kt
+++ app/src/main/java/com/example/nocternalflames/Activities/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.nocternalflames.Activities
 import android.content.ContentValues.TAG
 import android.media.Image
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.PersistableBundle
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nocternalflames.R
 import com.google.firebase.Firebase
@@ -17,6 +20,9 @@ import com.google.firebase.database.database
 class MainActivity : AppCompatActivity(){
     // Local candleState
     private var candlesState = arrayOf(false,false,false,false,false)
+    private var gameTime: Long = 600900 // 10 minutes
+    private var timeLeftInMillis: Long = 601000 // 10 minutes
+    private var countDownTimer: CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,12 +72,86 @@ class MainActivity : AppCompatActivity(){
                 candlesdb[index].setValue(candlesState[index].not())
             }
         }
+
+        // Start game button
+        val startButton = findViewById<Button>(R.id.startButton)
+        val startTimeRef = database.getReference("start time")
+
+        startButton.setOnClickListener {
+            startTimeRef.setValue(System.currentTimeMillis())
+        }
+
+        // Reset game button
+        val resetButton = findViewById<Button>(R.id.resetButton)
+
+        resetButton.setOnClickListener {
+            resetGame()
+        }
+
+        // Listen to candle values in the db
+        startTimeRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val targetTime = dataSnapshot.value as Long + gameTime
+                if(targetTime > System.currentTimeMillis()) {
+                    timeLeftInMillis = targetTime - System.currentTimeMillis()
+                    countDownTimer?.cancel()
+                    startTimer()
+                }
+                else{
+                    // if the game has not started yet
+                    countDownTimer?.cancel()
+                    timeLeftInMillis = gameTime
+                    println("asdf")
+                    updateTimer()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
     }
 
-    fun lightCandle(candle:ImageView, light: Boolean){
+    private fun lightCandle(candle:ImageView, light: Boolean){
         if (light)
             candle.setImageResource(R.drawable.candle_on)
         else
             candle.setImageResource(R.drawable.candle_off)
+    }
+
+    private fun startTimer() {
+        countDownTimer = object : CountDownTimer(timeLeftInMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeftInMillis = millisUntilFinished
+                updateTimer()
+            }
+
+            override fun onFinish() {
+                // Timer finished
+            }
+        }.start()
+    }
+
+    private fun updateTimer() {
+        val minutes = (timeLeftInMillis / 1000).toInt() / 60
+        val seconds = (timeLeftInMillis / 1000).toInt() % 60
+        val timeLeftFormatted = String.format("%02d:%02d", minutes, seconds)
+        val timerText = findViewById<TextView>(R.id.timerTextView)
+        timerText.text = timeLeftFormatted
+    }
+
+    private fun resetGame(){
+        // Firebase realtime database
+        val database = Firebase.database("https://nocturnal-lights-61563-default-rtdb.europe-west1.firebasedatabase.app")
+
+        database.getReference("candle1_state").setValue(false)
+        database.getReference("candle2_state").setValue(false)
+        database.getReference("candle3_state").setValue(false)
+        database.getReference("candle4_state").setValue(false)
+        database.getReference("candle5_state").setValue(false)
+        database.getReference("start time").setValue(0L)
     }
 }
